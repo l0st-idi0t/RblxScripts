b2ab49f845b834c3c100a5aed7831616805c3ef5
Carson <54319189+l0st-idi0t@users.noreply.github.com>
2022-01-07 22:55:35 -0600

    spam teleport kill

---
+++ spamKill.lua
@@ -0,0 +1,24 @@
+local toggle = false
+local UIS = game:GetService("UserInputService")
+
+local remote = game.ReplicatedStorage.meleeEvent
+local plr = game.Players
+local target = "Reading_isfun123"
+
+local function punch(name)
+  remote:FireServer(plr[name])
+end
+
+UIS.InputBegan:Connect(function(key, processed)
+	if processed then return end
+	
+	if key.KeyCode == Enum.KeyCode.F then
+		toggle = not toggle
+		
+		while toggle do
+			wait(0.1)
+			workspace:WaitForChild(game.Players.LocalPlayer.Name).HumanoidRootPart.CFrame = workspace:WaitForChild(target).HumanoidRootPart.CFrame
+			punch(target)
+		end
+	end
+end)
